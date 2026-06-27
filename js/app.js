330042767c7c9840cc40b1831e624ce282db1d8b
Dinujaya2001 <dinujayatharaka2001@gmail.com>
2026-06-27 14:27:42 +0530

    Refactor app.js: Comment out unused variables and console logs for clarity

--- js/app.js
+++ js/app.js
@@ -1,14 +1,30 @@
 
-let name = "tharaka";
-let age = 10;
-let city = "Galle";
-let Country = "Sri Lanka";
+// let name = "tharaka";
+// let age = 10;
+// let city = "Galle";
+// let Country = "Sri Lanka";
+
+// console.log(typeof name);
+// console.log(typeof age);
+// console.log(typeof city);
+// console.log(typeof Country);
+
+// alert("Successfully Rejistration!");
+
+let name = "saman";
+let age = 18;
+let isAfterAL = false;
+let customers = [];
+let customer ={};
+let salery = 7500.00;
 
 console.log(typeof name);
 console.log(typeof age);
-console.log(typeof city);
-console.log(typeof Country);
+console.log(typeof customers);
+console.log(typeof customer);
+console.log(typeof salery);
 
-alert("Successfully Rejistration!");
+
+
 
 
